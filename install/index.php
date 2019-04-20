364fab7ae6924a98cb33225a11f324ec22611698
aslanator <aslanator@mail.ru>
2019-04-20 16:08:41 +0300

    create install

---
+++ install/index.php
@@ -0,0 +1,84 @@
+<?php
+
+IncludeModuleLangFile(__FILE__);
+if (class_exists("excel2sql"))
+    return;
+
+class excel2sql extends CModule
+{
+    var $MODULE_ID = "excel2sql";
+    var $MODULE_VERSION;
+    var $MODULE_VERSION_DATE;
+    var $MODULE_NAME;
+    var $MODULE_DESCRIPTION;
+    var $MODULE_CSS;
+    var $MODULE_GROUP_RIGHTS = "N";
+
+    function excel2sql()
+    {
+        $arModuleVersion = array();
+        $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+        $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+        $this->MODULE_NAME = GetMessage("EXCEL2SQL_MODULE_NAME");
+        $this->MODULE_DESCRIPTION = GetMessage("EXCEL2SQL_MODULE_DESCRIPTION");
+    }
+
+    function InstallDB($arParams = array())
+    {
+        return true;
+    }
+
+    function UnInstallDB($arParams = array())
+    {
+        return true;
+    }
+
+    function InstallEvents()
+    {
+        return true;
+    }
+
+    function UnInstallEvents()
+    {
+        return true;
+    }
+
+    function InstallFiles($arParams = array())
+    {
+        CopyDirFiles($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/excel2sql/install/themes/", $_SERVER["DOCUMENT_ROOT"]."/bitrix/themes/", true, true);
+        return true;
+    }
+
+    function UnInstallFiles()
+    {
+        DeleteDirFiles($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/excel2sql/install/themes/.default/", $_SERVER["DOCUMENT_ROOT"]."/bitrix/themes/.default");
+        DeleteDirFilesEx("/bitrix/themes/.default/icons/excel2sql/");
+        return true;
+    }
+
+    function DoInstall()
+    {
+        global $USER, $APPLICATION;
+
+        if ($USER->IsAdmin())
+        {
+            if ($this->InstallDB())
+            {
+                $this->InstallEvents();
+                $this->InstallFiles();
+            }
+            $GLOBALS["errors"] = $this->errors;
+        }
+    }
+
+    function DoUninstall()
+    {
+        global $USER, $APPLICATION, $step;
+        if ($USER->IsAdmin())
+        {
+            $this->UnInstallDB();
+            $this->UnInstallEvents();
+            $this->UnInstallFiles();
+        }
+    }
+}
---
+++ lang/ru/install/index.php
@@ -0,0 +1,4 @@
+<?php
+
+$MESS['EXCEL2SQL_MODULE_NAME'] = "Excel to SQL";
+$MESS['EXCEL2SQL_MODULE_DESCRIPTION'] = "Создает таблицы в базе данных и ORM классы на основе xls(x) файла";
