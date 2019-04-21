61d2947fcdbea35b44a6770ce68001944973cf76
aslanator <aslanator@mail.ru>
2019-04-21 17:04:39 +0300

    add phpdoc to excel2sqlIndex

--- admin/classes/excel2sqlIndex.php
+++ admin/classes/excel2sqlIndex.php
@@ -34,6 +34,9 @@ class Excel2SqIndex {
      */
     protected $lAdmin = null;
 
+    /**
+     * Check rights and dependencies. Initialize data for display table
+     */
     public function __construct()
     {
         global $APPLICATION, $USER;
@@ -51,6 +54,9 @@ class Excel2SqIndex {
         $this->initView();
     }
 
+    /**
+     * Initialize all data to display table
+     */
     protected function initView(){
         global $APPLICATION;
 
@@ -60,12 +66,15 @@ class Excel2SqIndex {
         $oSort = new CAdminSorting($this->sTableID, false, false);
         $this->lAdmin = new CAdminList($this->sTableID, $oSort);
 
-        $this->showContextMenu();
-        $this->showList();
+        $this->setContextMenu();
+        $this->setList();
         $this->lAdmin->CheckListMode();
     }
 
-    protected function showContextMenu(){
+    /**
+     * add button that leads to page at which we add documents
+     */
+    protected function setContextMenu(){
         $aContext = array(
             array(
                 "TEXT"	=> Loc::getMessage("ADD_EXCEL_DOCUMENTS"),
@@ -94,6 +103,9 @@ class Excel2SqIndex {
         $_SESSION["SESS_SORT_ORDER"][$uniq] = $order;
     }
 
+    /**
+     * @return array
+     */
     protected function getSort(){
         global $by, $order;
         $options = CUserOptions::GetOption("list", $this->sTableID);
@@ -107,7 +119,9 @@ class Excel2SqIndex {
             $order = $order ?: $options['order'];
         }
 
-        $by = strtoupper($by);
+        $by = $by ? strtoupper($by) : 'ID';
+        $order = $order ? strtoupper($order) : 'ASC';
+
 
         return [$by => $order];
     }
@@ -117,27 +131,43 @@ class Excel2SqIndex {
         return intval($options['page_size']) > 0 ? intval($options['page_size']) : self::DEFAULT_PAGE_SIZE;
     }
 
-    protected function showList(){
-        $sort = $this->getSort();
-
-        $classList = Excel2sqlTable::getAll($sort);
-
+    /**
+     * set table headers
+     */
+    protected function addHeaders(){
         $this->lAdmin->AddHeaders(array(
             array("id"=>"ID", "content"=>Loc::GetMessage("EXCEL2SQL_ENTITY_ID_FIELD"), "sort"=>"id", "default"=>true),
             array("id"=>"SORT","content"=>Loc::GetMessage("EXCEL2SQL_ENTITY_SORT_FIELD"), "sort"=>"sort", "default"=>true),
             array("id"=>"TABLE_NAME", "content"=>Loc::GetMessage("EXCEL2SQL_ENTITY_TABLE_NAME_FIELD"), "sort"=>"table_name", "default"=>true),
             array("id"=>"ORM_PATH", "content"=>Loc::GetMessage("EXCEL2SQL_ENTITY_ORM_PATH_FIELD"), "sort"=>"orm_path", "default"=>true),
         ));
+    }
+
+    /**
+     * set rows, which output in display method
+     */
+    protected function setList(){
+        $sort = $this->getSort();
+
+        $classList = Excel2sqlTable::getAll($sort);
+
+        $this->addHeaders();
 
         while($arClass = $classList->fetch())
         {
-            $row =& $this->lAdmin->AddRow($arClass['ID'], $arClass['ID']);
-            $row->AddViewField("ID", $arClass['ID']);
-            $row->AddViewField("SORT", $arClass['SORT']);
-            $row->AddViewField("TABLE_NAME", $arClass['TABLE_NAME']);
-            $row->AddViewField("ORM_PATH", $arClass['ORM_PATH']);
+            $this->addRow($arClass);
         }
+    }
 
+    /**
+     * @param array $arClass
+     */
+    protected function addRow(Array $arClass){
+        $row =& $this->lAdmin->AddRow($arClass['ID'], $arClass['ID']);
+        $row->AddViewField("ID", $arClass['ID']);
+        $row->AddViewField("SORT", $arClass['SORT']);
+        $row->AddViewField("TABLE_NAME", $arClass['TABLE_NAME']);
+        $row->AddViewField("ORM_PATH", $arClass['ORM_PATH']);
     }
 
     public function display(){
