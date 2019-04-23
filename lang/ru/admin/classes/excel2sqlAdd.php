dd73336c79fc2672ad2b8dd814861ff472d067d5
aslanator <aslanator@mail.ru>
2019-04-24 00:17:49 +0300

    add success message

--- lang/ru/admin/classes/excel2sqlAdd.php
+++ lang/ru/admin/classes/excel2sqlAdd.php
@@ -5,3 +5,4 @@ $MESS['GO_BACK'] = "Назад";
 $MESS['MAIN_TAB'] = "Документ";
 $MESS['ADD_DOCUMENT'] = "Добавить";
 $MESS['TABLE ALREADY EXISTS'] = "Таблица {TABLE} уже существует;";
+$MESS['SUCCESS'] = 'Успешно';
